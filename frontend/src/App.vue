94ca8f2fd46f8736b78132e8a4997e33608591a6
ChenWanchun <2696992931@qq.com>
2023-11-30 22:19:31 +0800

    剩下的界面提交

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,18 +4,33 @@
   <!-- <Main /> -->
   <!-- <outputView/> -->
   <!-- <inputView/> -->
+  <!-- <accountSettings /> -->
+  <!-- <feedback /> -->
+  <!-- <version /> -->
+  <!-- <myInfo /> -->
+  <!-- <secure /> -->
 </template>
 <script>
 import Login from './views/Login.vue'
 import Main from './views/Main.vue'
 import outputView from "./views/outputView.vue"
 import inputView from "./views/inputView.vue"
+import accountSettings from "./views/accountSettings.vue"
+import feedback from "./views/feedback.vue"
+import version from "./views/version.vue"
+import myInfo from "./views/myInfo.vue"
+import secure from "./views/secure.vue"
 export default {
   components: {
     Login,
     Main,
     outputView,
-    inputView
+    inputView,
+    accountSettings,
+    feedback,
+    version,
+    myInfo,
+    secure
   }
 }
 </script>
